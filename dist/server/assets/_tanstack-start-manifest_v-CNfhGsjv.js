086a9bae733604bca9c20ba89c5b88a4622ba5d2
Jennie Alles <jennieramida@gmail.com>
2025-11-25 01:19:04 +0700

    fix: html

--- dist/server/assets/_tanstack-start-manifest_v-CNfhGsjv.js
+++
@@ -1,4 +0,0 @@
-const tsrStartManifest = () => ({ "routes": { "__root__": { "filePath": "/Users/jenniealles/Desktop/initia-repo/iusd-landing-page/src/routes/__root.tsx", "children": ["/"], "preloads": ["/assets/main-CEixBvan.js"], "assets": [{ "tag": "link", "attrs": { "rel": "stylesheet", "href": "/assets/main-Cs6Ic0o-.css", "type": "text/css" } }] } }, "clientEntry": "/assets/main-CEixBvan.js" });
-export {
-  tsrStartManifest
-};
